d4d315d4cfbf1141f4f4da0661cec1a0c475fe7f
Selvaraj Kesavan <92854921+Selva79@users.noreply.github.com>
2022-05-20 08:51:03 +0530

    Update DevOpsExample.java

--- DevOpsExample.java
+++ DevOpsExample.java
@@ -4,6 +4,6 @@
  */
 public class DevOpsExample {
   public static void main(String[] args) {
-    System.out.println("Welcome to WIMS DevOps Session 5");
+    System.out.println("Welcome to DevOps.");
   }
 }
